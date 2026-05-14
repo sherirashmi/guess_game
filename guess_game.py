7569d1d488b2f671f4ffe74b171cbc3dadeec4b6
Rashmi Reddy Sheri <srashmireddy7@gmail.com>
2026-05-14 13:49:49 +0200

    add

---
+++ guess_game.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, session
+import random
+
+app = Flask(__name__)
+app.secret_key = "secret123"
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+
+    if "number" not in session:
+        session["number"] = random.randint(1, 100)
+
+    message = ""
+
+    if request.method == "POST":
+
+        guess = int(request.form["guess"])
+        our_num = session["number"]
+
+        if guess < our_num:
+            message = "Guess a higher number"
+
+        elif guess > our_num:
+            message = "Guess a lower number"
+
+        else:
+            message = "You guessed it right!"
+
+            # Start new game
+            session["number"] = random.randint(1, 100)
+
+    return render_template("index.html", message=message)
+
+if __name__ == "__main__":
+    app.run(debug=True)
